3a493d8258d0e64d15c7d75498edf8ce746cd84a
samuel <samuelkamotho92@gmail.com>
2022-03-22 10:12:32 +0300

    created a Navabar and a footer component

---
+++ client/src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="footer">
+            <h2>2022 © All Righ reserved</h2>
+        </div>
+     );
+}
+ 
+export default Footer;
---
+++ client/src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Depertments</li>
+            <li>Families</li>
+            <li>Contact Us</li>
+            <li>Login</li>
+            <li>Sign Up</li>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Navbar;
