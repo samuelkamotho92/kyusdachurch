cd8d6564675b1729639a1141929d41e9270869f1
samuel <samuelkamotho92@gmail.com>
2022-04-26 21:50:23 +0300

    added post form

---
+++ client/src/components/Posts/posts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Post from "./Post/post";
+const Posts = () => {
+    return ( 
+        <>
+    <Post />
+     <Post />
+     <Post />
+        </>
+     );
+}
+ 
+export default Posts;
